ce549f001607d0be257fbe9936873f4a530cb251
revisitor <mikado.brave@gmail.com>
2024-07-16 14:26:17 +0700

    create pick function

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -1,9 +1,19 @@
 /**
  * pick - Creates an object composed of the picked object properties:
  * @param {object} obj - the source object
- * @param {...string} fields - the properties paths to pick
+ * @param {...string} keys - the properties paths to pick
  * @returns {object} - returns the new object
  */
-export const pick = (obj, ...fields) => {
+export const pick = (obj, ...keys) => {
+  const result = {};
 
+  for (const key of keys) {
+    const value = obj[key];
+
+    if (value != null) {
+      result[key] = value;
+    }
+  }
+
+  return result;
 };
