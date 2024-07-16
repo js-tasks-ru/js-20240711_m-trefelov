3535886c20740742f84153bc2881d4f62a951fd8
revisitor <mikado.brave@gmail.com>
2024-07-16 14:46:03 +0700

    create omit function

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -1,9 +1,17 @@
 /**
  * omit - creates an object composed of enumerable property fields
  * @param {object} obj - the source object
- * @param {...string} fields - the properties paths to omit
+ * @param {...string} keys - the properties paths to omit
  * @returns {object} - returns the new object
  */
-export const omit = (obj, ...fields) => {
+export const omit = (obj, ...keys) => {
+  const result = { ...obj };
 
+  for (const key of keys) {
+    if (Object.hasOwn(result, key)) {
+      delete result[key];
+    }
+  }
+
+  return result;
 };
