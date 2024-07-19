3145389fd48d9fba5727aabaaca6a651a6dab8b6
revisitor <mikado.brave@gmail.com>
2024-07-19 15:09:21 +0700

    create invertObj function

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -1,8 +1,14 @@
 /**
  * invertObj - should swap object keys and values
  * @param {object} obj - the initial object
- * @returns {object | undefined} - returns new object or undefined if nothing did't pass
+ * @returns {object | undefined} - returns new object or undefined if nothing didn't pass
  */
 export function invertObj(obj) {
+  if (obj == null) {
+    return;
+  }
 
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [value, key])
+  );
 }
